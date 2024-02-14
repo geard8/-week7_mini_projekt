d48ed1f67f7e889571a9defb6a616bbe74538e57
Nicklas Thor <nicke.tor.86@gmail.com>
2024-02-14 15:57:40 +0100

    first commit

---
+++ week7_mini_projekt/Class1.cs
@@ -0,0 +1,25 @@
+﻿
+namespace week7_mini_projekt
+{
+    abstract class Asset
+    {
+        public DateTime date; // date of Asset
+        public float price; // cost of Asset
+        public string name; // product name of Asset
+        private string office; // office where Asset is.
+        public string Office
+        {
+            get { return office; }
+            // set office as long as it is "Stockholm" or "Tokyo" or "Boston"
+            set
+            {
+                if (value != "Stockholm" && value != "Tokyo" && value != "Boston") 
+                { 
+                    throw new ArgumentException("office and only be \"Stockholm\" or \"Tokyo\" or \"Boston\""); 
+                }
+                else { office = value; }
+
+            }
+        }
+    }
+}
