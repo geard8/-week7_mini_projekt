fa92ed4f7d7b6c17336ce12022c13c4efc74fb25
Nicklas Thor <nicke.tor.86@gmail.com>
2024-02-16 18:23:06 +0100

    Update Display.cs to be more in line of this project.
    - Update Display.cs to be more in line of this project, by removing blue as a color and update comment of red and yellow.

--- week7_mini_projekt/Display.cs
+++ week7_mini_projekt/Display.cs
@@ -5,7 +5,7 @@
         // DisplayColorMsg is used to write out text to consol with a selection pre defined amount of color ment to be used for diffrent purpose.
         public static void DisplayColorMsg(string msg, string color)
         {
-            // red is ment to be used for misstakes or errors, ex something did not validate.
+            // red is ment to be used for misstakes or errors, ex something did not validate. Or red is ment for Asset that are 3 month or less in list of asset
             if (color == "red")
             {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -20,14 +20,9 @@
                 Console.WriteLine(msg);
                 Console.ResetColor();
             }
-            // blue is ment to be used to highlight something to draw attaction to it.
-            else if (color == "blue")
-            {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine(msg);
-                Console.ResetColor();
-            }
-            // yellow is ment for testing i shall not show up in public version unless something went wrong.
+
+            // yellow is ment for Asset that are 6 month or less in list of asset unless it less then 3, then it is red that is used.
+            // Or yellow is ment for testing i shall not show up in public version unless something went wrong.
             else if (color == "yellow")
             {
                 Console.ForegroundColor = ConsoleColor.Yellow;
